43cd0246bbc244fee3bc5fd04e10b8196de65a69
haziqilham <71216770+haziqilham@users.noreply.github.com>
2021-11-07 19:37:19 +0800

    swapped to mailjet (yet to rename)

--- web/sendgrid.php
+++ web/sendgrid.php
@@ -1,48 +1,27 @@
 <?php
-
-require 'vendor/autoload.php';
-// Dotenv::load(__DIR__);
-$sendgrid_apikey = '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';
-$sendgrid = new SendGrid($sendgrid_apikey);
-$url = 'https://api.sendgrid.com/';
-$pass = $sendgrid_apikey;
-$template_id = '<your_template_id>';
-$js = array(
-  'sub' => array(':name' => array('Elmer')),
-  'filters' => array('templates' => array('settings' => array('enable' => 1, 'template_id' => $template_id)))
-);
-
-$params = array(
-    'to'        => "test@example.com",
-    'toname'    => "Example User",
-    'from'      => "you@youremail.com",
-    'fromname'  => "Your Name",
-    'subject'   => "PHP Test",
-    'text'      => "I'm text!",
-    'html'      => "<strong>I'm HTML!</strong>",
-    'x-smtpapi' => json_encode($js),
-  );
-
-$request =  $url.'api/mail.send.json';
-
-// Generate curl request
-$session = curl_init($request);
-// Tell PHP not to use SSLv3 (instead opting for TLS)
-curl_setopt($session, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
-curl_setopt($session, CURLOPT_HTTPHEADER, array('Authorization: Bearer ' . $sendgrid_apikey));
-// Tell curl to use HTTP POST
-curl_setopt ($session, CURLOPT_POST, true);
-// Tell curl that this is the body of the POST
-curl_setopt ($session, CURLOPT_POSTFIELDS, $params);
-// Tell curl not to return headers, but do return the response
-curl_setopt($session, CURLOPT_HEADER, false);
-curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
-
-// obtain response
-$response = curl_exec($session);
-curl_close($session);
-
-// print everything out
-print_r($response);
-
+  require 'vendor/autoload.php'; 
+  use \Mailjet\Resources;
+  $mj = new \Mailjet\Client('79de86f0cfcc4d0a342b20056cb017ab','06e1911f6e0ac49a6ececd9641b484a9',true,['version' => 'v3.1']);
+  $body = [
+    'Messages' => [
+      [
+        'From' => [
+          'Email' => "muhammadi.2019@scis.smu.edu.sg",
+          'Name' => "haziq"
+        ],
+        'To' => [
+          [
+            'Email' => "muhammadi.2019@scis.smu.edu.sg",
+            'Name' => "haziq"   
+          ]
+        ],
+        'Subject' => "TEST 4",
+        'TextPart' => "My first Mailjet email",
+        'HTMLPart' => "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
+        'CustomID' => "AppGettingStartedTest"
+      ]
+    ]
+  ];
+  $response = $mj->post(Resources::$Email, ['body' => $body]);
+  $response->success() && var_dump($response->getData());
 ?>
